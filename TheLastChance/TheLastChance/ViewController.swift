494b76c52f96a57eeb0391e163d191ba6a4da9d9
alexfedsv <alfedsv@yandex.ru>
2024-10-12 12:58:13 +0300

    init commit

---
+++ TheLastChance/TheLastChance/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TheLastChance
+//
+//  Created by  Alexander Fedoseev on 12.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
